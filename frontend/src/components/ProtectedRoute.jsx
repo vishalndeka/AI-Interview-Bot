b5ae98fd6a3abce98c1ff80f2425321c76140b34
vishalndeka <72727433+vishalndeka@users.noreply.github.com>
2024-09-04 01:51:58 +0530

    Created login and register pages Added form and simple style sheet

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -4,6 +4,7 @@ import { Navigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import api from "../api";
 import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
+import { useState, useEffect } from "react";
 
 function ProtectedRoute({ children }) {
   // check if user is authorised before we allow someone to access this route
@@ -11,9 +12,49 @@ function ProtectedRoute({ children }) {
 
   const [isAuthorized, setIsAuthorized] = useState(null);
 
-  const refershToken = async () => {};
+  useEffect(() => {
+    auth().catch(() => setIsAuthorized(false));
+  }, []);
 
-  const auth = async () => {};
+  const refershToken = async () => {
+    const refershToken = localStorage.getItem(REFRESH_TOKEN);
+    try {
+      // sending it to the backend
+      const res = await api.post("/api/token/refresh/", {
+        refresh: refershToken,
+      });
+      // if request is successful, i.e. access token received
+      if (res.status == 200) {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        setIsAuthorized(true);
+      } else {
+        setIsAuthorized(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsAuthorized(false);
+    }
+  };
+
+  // this runs first
+  const auth = async () => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (!token) {
+      setIsAuthorized(false);
+      return;
+    }
+    const decoded = jwtDecode(token);
+    const tokenExpiration = decoded.exp;
+    const now = Date.now() / 1000;
+
+    if (tokenExpiration < now) {
+      // if token's expired, refresh it
+      await refershToken();
+    } else {
+      // if token isnt expired, all good
+      setIsAuthorized(true);
+    }
+  };
 
   if (isAuthorized == null) {
     return <div>Loading...</div>;
@@ -21,3 +62,5 @@ function ProtectedRoute({ children }) {
 
   return isAuthorized ? children : <Navigate to="/login" />;
 }
+
+export default ProtectedRoute;
