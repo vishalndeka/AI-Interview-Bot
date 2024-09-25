255a748866e2da1c13c6e1377777e5338dc46698
vishalndeka <72727433+vishalndeka@users.noreply.github.com>
2024-09-25 11:04:19 +0530

    deleted django-react structure Changing to a FastAPI-react project

--- frontend/src/constants.js
+++
@@ -1,5 +0,0 @@
-// Constants to be used in API.js
-
-// access and refresh token will be stored in the browser 'locally' this key will help to retrieve it
-export const ACCESS_TOKEN = "access";
-export const REFRESH_TOKEN = "refresh";
--- frontend/src/pages/Login.jsx
+++
@@ -1,7 +0,0 @@
-import Form from "../components/Form";
-
-function Login() {
-  return <Form route="api/user/token/" method="login" />;
-}
-
-export default Login;
